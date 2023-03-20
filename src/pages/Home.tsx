97fc0290fcc4cf062350aea5fdc142ef9a435d63
hqunted <berkekirci@gmail.com>
2023-03-20 16:39:04 +0100

    feat: delete unnecessary_cl

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { Carousel } from "../components/Carousel";
 import { HomeStyles } from "../styles/HomeStyles";
 import { Direction } from "../components/Carousel";
 import { QuestionMark } from "../components/QuestionMark";
-import { getDotaData } from "../services/dotaHeroApi";
 
 export const Home = () => {
   return (
