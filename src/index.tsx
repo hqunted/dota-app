f2c3968806576c7415d430147d6d7a76d460c8c5
hqunted <berkekirci@gmail.com>
2023-03-20 14:35:49 +0100

    feat: push github

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { HeroMenu } from "./pages/HeroMenu";
 import { RouteLayout } from "./layouts/RouteLayout";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/dota-app" element={<RouteLayout />}>
-        <Route index element={<Home />}></Route>
-        <Route path="HeroMenu" element={<HeroMenu />}></Route>
-      </Route>
-    </Routes>
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/dota-app" element={<RouteLayout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="HeroMenu" element={<HeroMenu />}></Route>
+        </Route>
+      </Routes>
+    </HashRouter>
   </BrowserRouter>
 );
