669ccf04ea079a9ac2b071dd2cc6774298fbdfc5
hqunted <berkekirci@gmail.com>
2023-03-20 16:15:07 +0100

    feat: try-api-work

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import { Carousel } from "../components/Carousel";
 import { HomeStyles } from "../styles/HomeStyles";
 import { Direction } from "../components/Carousel";
 import { QuestionMark } from "../components/QuestionMark";
+import { getDotaData } from "../services/dotaHeroApi";
 
+console.log(getDotaData());
 export const Home = () => {
   return (
     <div className={HomeStyles.homeScreen.homeScreenBackground}>
