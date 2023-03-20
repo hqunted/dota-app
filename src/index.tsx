ed9782dcd589020c77886d1a1aa11ee76ab414e4
hqunted <berkekirci@gmail.com>
2023-03-20 15:06:23 +0100

    feat: fix-netlify

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,13 @@ import { Home } from "./pages/Home";
 import { HeroMenu } from "./pages/HeroMenu";
 import { RouteLayout } from "./layouts/RouteLayout";
 
-
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/dota-app" element={<RouteLayout />}>
+      <Route path="/" element={<RouteLayout />}>
         <Route index element={<Home />}></Route>
         <Route path="HeroMenu" element={<HeroMenu />}></Route>
       </Route>
